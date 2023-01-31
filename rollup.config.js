4de694b951506e8e0668c80c5dcc55aab6efc400
Akihiko Koizuka <koizuka@dwango.co.jp>
2023-01-31 17:24:58 +0900

    use @rollup/plugin-terser instead of rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
 import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 
 export default [
   // ES Modules
